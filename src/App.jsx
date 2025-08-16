7a02b8a2a0c6ff747a314b416f2d7b2f5d9c1c18
iman <imancx.cx@gmail.com>
2025-08-16 13:10:33 +0330

    add gsap in heroSection container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
+//gsap
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
