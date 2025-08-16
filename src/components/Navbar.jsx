bdddeae1eb83b60b656b79861e7a22a92ccba475
iman <imancx.cx@gmail.com>
2025-08-16 12:15:43 +0330

    Navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+      <img src="/images/nav-logo.svg" alt="nav-logo" className="md:w-24 w-20" />
+    </nav>
+  );
 };
 
 export default Navbar;
